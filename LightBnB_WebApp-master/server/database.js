5e7944aa7ad2e3624293dab26c5528d70ac9afe0
Sam Yang <samyang8989@gmail.com>
2020-08-21 03:01:47 +0000

    Newly created listing shows up on My Listings

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -112,7 +112,7 @@ const getAllProperties = function (options, limit = 10) {
 	let queryString = `
 		SELECT properties.*, AVG(property_reviews.rating) AS average_rating
 		FROM properties 
-		JOIN property_reviews ON properties.id = property_id
+		LEFT JOIN property_reviews ON properties.id = property_id
 	`;
 
 	if (options.city) {
